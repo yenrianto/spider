7e8e7d43cc149ab8f0013495126864f5be4530b5
seven <7b37b3@gmail.com>
2026-06-04 22:32:40 -0300

    fix: update sqlite_driver_impl build.zig paths lib/ → src/

--- modules/sqlite/sqlite_driver_impl/build.zig
+++ modules/sqlite/sqlite_driver_impl/build.zig
@@ -7,10 +7,10 @@ pub fn build(b: *std.Build) !void {
     const default_sqlite3_build = [_][]const u8{"-std=c99"};
     const sqlite3_build = b.option([]const []const u8, "sqlite3", "options to use when compiling sqlite3") orelse &default_sqlite3_build;
 
-    const lib_path = b.path("lib");
+    const lib_path = b.path("src");
 
     const translate_c = b.addTranslateC(.{
-        .root_source_file = b.path("lib/sqlite3.h"),
+        .root_source_file = b.path("src/sqlite3.h"),
         .target = target,
         .optimize = optimize,
     });
@@ -35,7 +35,7 @@ pub fn build(b: *std.Build) !void {
     });
     mod_sqlite.addIncludePath(lib_path);
     mod_sqlite.addCSourceFile(.{
-        .file = b.path("lib/sqlite3.c"),
+        .file = b.path("src/sqlite3.c"),
         .flags = sqlite3_build,
     });
 
